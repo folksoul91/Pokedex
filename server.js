9df7df60f1398803a387414011904d1eaa7de7e3
Dane Kim <mr.dane@danes-mbp.lan>
2022-05-19 22:48:08 -0400

    completed mvp for this hw

--- server.js
+++ server.js
@@ -7,18 +7,48 @@ const pokemon = require("./models/pokemon");
 // Middlewares
 const methodOverride = require("method-override");
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride("_method"));
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 // Index route
 app.get("/pokemon", (req, res) => {
-  res.render("index.ejs", { data: pokemon });
+  res.render("index.ejs", { pokeData: pokemon });
+});
+
+//Delete route
+app.delete("/pokemon/:indexOfPokemonArray", (req, res) => {
+  pokemon.splice(req.params.indexOfPokemonArray, 1);
+  res.redirect("/pokemon");
+});
+
+//Edit route
+app.get("/pokemon/:indexOfPokemonArray/edit", (req, res) => {
+  res.render("edit.ejs", {
+    pokedex: pokemon[req.params.indexOfPokemonArray],
+    index: req.params.indexOfPokemonArray,
+  });
+});
+//Update route
+app.put("/pokemon/:indexOfPokemonArray", (req, res) => {
+  const updatePokemon = {
+    img: "https://cdn-icons-png.flaticon.com/512/528/528101.png",
+    name: req.body.name,
+    type: req.body.type,
+    stats: {
+      hp: req.body.hp,
+      attack: req.body.attack,
+      defense: req.body.defense,
+    },
+  };
+  pokemon[req.params.indexOfPokemonArray] = updatePokemon;
+  res.redirect("/pokemon");
 });
 
 // New route
 app.get("/pokemon/new", (req, res) => {
   res.render("new.ejs");
 });
+//Create route
 app.post("/pokemon", (req, res) => {
   const createNewPokemon = {
     img: "https://cdn-icons-png.flaticon.com/512/528/528101.png",
@@ -26,7 +56,6 @@ app.post("/pokemon", (req, res) => {
     type: req.body.type,
     stats: {
       hp: req.body.hp,
-      speed: req.body.speed,
       attack: req.body.attack,
       defense: req.body.defense,
     },
@@ -34,23 +63,8 @@ app.post("/pokemon", (req, res) => {
   pokemon.push(createNewPokemon);
   res.redirect("/pokemon");
 });
-//Delete route
-app.delete("/pokemon/:indexOfPokemonArray", (req, res) => {
-  pokemon.splice(req.params.indexOfPokemonArray, 1);
-  res.redirect("/pokemon");
-});
-//U
 
-//C
-
-//E
-app.get("/pokemon/:indexOfPokemonArray/edit", (req, res) => {
-  res.render("edit.ejs", {
-    pokesingle: pokemon[req.params.indexOfPokemonArray],
-    index: req.params.indexOfPokemonArray,
-  });
-});
-//S
+//Show route
 app.get("/pokemon/:id", (req, res) => {
   res.render("show.ejs", { poke: pokemon[req.params.id] });
 });
